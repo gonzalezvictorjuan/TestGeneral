9994c9d33abecc3b6d3da195d3ae893db420438c
Victor Juan Gonzalez <victor.gonzalez@globant.com>
2022-09-29 19:16:07 -0300

    Hello World

---
+++ index.js
@@ -0,0 +1,5 @@
+function greeter(person) {
+    return "Hello ".concat(person, "!");
+}
+var n = 'World';
+console.log(greeter(n));
---
+++ index.ts
@@ -0,0 +1,7 @@
+function greeter(person: string) {
+  return `ello ${person}!`;
+}
+
+const n = 'World';
+
+console.log(greeter(n));
